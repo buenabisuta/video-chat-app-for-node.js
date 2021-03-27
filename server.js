fa0a3f343f1039db29f40d14313239fb25ab049c
buenabisuta <buenabisuta765@gmail.com>
2021-03-27 15:07:52 +0900

    シグナリングサーバーの実装

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { v4: uuidV4 } = require("uuid");
 server.listen(process.env.PORT || 3030);
 
 app.set("view engine","ejs");
-app.use(express.static("/public"));
+app.use(express.static("public"));
 
 app.get("/", (req,res) => {
   res.redirect(`/${uuidV4()}`);
@@ -17,3 +17,9 @@ app.get("/:room", (req,res) => {
   res.render("room", { roomId: req.params.room });
 });
 
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId,userId) => {
+    socket.join(roomId);
+    socket.to(roomId).emit("user-connected", userId);
+  });
+});

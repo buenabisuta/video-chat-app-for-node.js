3a5ef3158cd690d49400bd344e5762c8c2391536
buenabisuta <buenabisuta765@gmail.com>
2021-03-27 15:29:31 +0900

    カメラ表示の実装

--- public/script.js
+++ public/script.js
@@ -1,5 +1,23 @@
 const socket = io("/");
 const myPeer = new Peer();
+const videoWrap = document.getElementById("video-wrap");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata",() => {
+    video.play();
+  })
+  videoWrap.append(video);
+};
+
+navigator.mediaDevices.getUserMedia({
+  video: true,
+  audio: true,
+}).then((stream) => {
+  addVideoStream(myVideo,stream);
+});
 
 myPeer.on("open", (userId) => {
   socket.emit("join-room", ROOM_ID,userId);
